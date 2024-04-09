d1e9b7ae5e3a069b5171d0707fa407697aabbe93
Alonso <81324208+Alonso256@users.noreply.github.com>
2024-04-09 12:31:36 +0200

    Creación esquema basico MVC

---
+++ Aeroluxe/vista/pie.php
@@ -0,0 +1,28 @@
+<footr>
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+
+    </div>
+</footr>
+
+
+
+<!-- Javascript files-->
+<script src="<?php echo URL ?>/resources/js/jquery.min.js"></script>
+<script src="<?php echo URL ?>/resources/js/popper.min.js"></script>
+<script src="<?php echo URL ?>/resources/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo URL ?>/resources/js/jquery-3.0.0.min.js"></script>
+<script src="<?php echo URL ?>/resources/js/plugin.js"></script>
+<!-- sidebar -->
+<script src="<?php echo URL ?>/resources/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="<?php echo URL ?>/resources/js/custom.js"></script>
+<script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+
+
+</body>
+
+</html>
