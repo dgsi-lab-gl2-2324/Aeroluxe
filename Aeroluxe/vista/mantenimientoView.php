cbb1bc3ba500a107c618abba7b6bc6ce685894ef
Alonso <81324208+Alonso256@users.noreply.github.com>
2024-04-22 19:38:05 +0200

    Tabla de mantenimiento

---
+++ Aeroluxe/vista/mantenimientoView.php
@@ -0,0 +1,60 @@
+<section id="blog" class="blog">
+    <div class="container" data-aos="fade-up">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h1 class="text-primary text-center">MANTENIMIENTO</h1>
+
+            </div>
+            <table>
+    <thead>
+        <tr>
+            <th>Nombre Trabajador</th>
+            <th>Área Mantener</th>
+            <th>Tipo de Mantenimiento</th>
+            <th>Última Fecha de Mantenimiento</th>
+            <th>Próxima Revisión</th>
+        </tr>
+    </thead>
+    <tbody>
+    <tr>
+            <td>Juan Pérez</td>
+            <td>Maquinaria</td>
+            <td>Preventivo</td>
+            <td>2028-01-15</td>
+            <td>2028-07-20</td>
+        </tr>
+        <tr>
+            <td>Maria López</td>
+            <td>Imanes de Sustentación</td>
+            <td>Correctivo</td>
+            <td>2028-02-11</td>
+            <td>2028-05-18</td>
+        </tr>
+        <tr>
+            <td>Ricardo Gómez</td>
+            <td>Vehículos Aéreos</td>
+            <td>Revisión General</td>
+            <td>2028-01-20</td>
+            <td>2028-06-22</td>
+        </tr>
+        <tr>
+            <td>Sofía Castro</td>
+            <td>Piscinas y Filtración</td>
+            <td>Mantenimiento de Rutina</td>
+            <td>2028-01-10</td>
+            <td>2028-07-15</td>
+        </tr>
+        <tr>
+            <td>Carlos Ruiz</td>
+            <td>Imanes de Sustentación</td>
+            <td>Inspección Detallada</td>
+            <td>2028-01-25</td>
+            <td>2028-07-25</td>
+        </tr>
+    </tbody>
+</table>
+        </div>
+    </div>
+</section>
